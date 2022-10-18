4eb664653ec47a00888f3cca2c53643f3d5f21fa
horikz <64258321+horikz@users.noreply.github.com>
2022-10-18 13:39:48 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,13 +1,19 @@
-def buildApp() {
+def buildJar() {
     echo "Building"
+    sh "mvn package"
 }
 
-def testApp() {
-    echo "Testing"
+def buildImage() {
+    echo "Build docker image"
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {     
+        sh 'docker build -t horikz/java-maven-app:jma-2.0 .'   
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push horikz/java-maven-app:jma-2.0'
+    }
 }
 
 def deployApp() {
-    echo "Deploying version ${params.VERSION}"
+    echo "Deploying the app"
 }
 
 return this
